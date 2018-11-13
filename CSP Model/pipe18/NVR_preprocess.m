c7bc276918c503d76f2425ee4f46a4e195521beb
eioe <felix_klotzsche@web.de>
2018-11-13 17:06:42 +0100

    ICA issues

--- CSP Model/pipe18/NVR_preprocess.m
+++ CSP Model/pipe18/NVR_preprocess.m
@@ -5,14 +5,14 @@
 NVR_path = genpath('..\..\..\NeVRo');
 addpath(NVR_path);
 
-m_conds = {'mov'}; %{'nomov'}; %{'mov' 'nomov'};
+m_conds = {'nomov'}; % {'mov'}; % {'mov' 'nomov'}; %
 c_styles = {'SBA'}; %{'SBA', 'SA'};
 
 for mc=1:numel(m_conds)
     for cs = 1:numel(c_styles)
         %NVR_03_crop(c_styles{cs},m_conds{mc});%('SBA','mov')
         %NVR_04_eventsARO(c_styles{cs},m_conds{mc});%('SBA','mov')
-        NVR_05_ICA(c_styles{cs},m_conds{mc});
+        %NVR_05_ICA(c_styles{cs},m_conds{mc});
         %NVR_06_rejcomp(c_styles{cs},m_conds{mc});
     end
 end
