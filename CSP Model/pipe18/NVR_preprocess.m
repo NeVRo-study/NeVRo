99a65424d161ab8a84c036271f0f27fb4744c9d9
eioe <felix_klotzsche@web.de>
2018-11-14 10:33:04 +0100

    resolved conflicts in NVR_preprocess.m

--- CSP Model/pipe18/NVR_preprocess.m
+++ CSP Model/pipe18/NVR_preprocess.m
@@ -5,11 +5,7 @@
 NVR_path = genpath('..\..\..\NeVRo');
 addpath(NVR_path);
 
-<<<<<<< HEAD
-m_conds = {'nomov'}; % {'mov'}; % {'mov' 'nomov'}; %
-=======
 m_conds = {'mov' 'nomov'}; %{'nomov'}; % {'mov'}; % 
->>>>>>> refs/remotes/origin/master
 c_styles = {'SBA'}; %{'SBA', 'SA'};
 
 tic
@@ -17,11 +13,7 @@ for mc=1:numel(m_conds)
     for cs = 1:numel(c_styles)
         %NVR_03_crop(c_styles{cs},m_conds{mc});%('SBA','mov')
         %NVR_04_eventsARO(c_styles{cs},m_conds{mc});%('SBA','mov')
-<<<<<<< HEAD
-        %NVR_05_ICA(c_styles{cs},m_conds{mc});
-=======
         NVR_05_ICA(c_styles{cs},m_conds{mc}, 'binica');
->>>>>>> refs/remotes/origin/master
         %NVR_06_rejcomp(c_styles{cs},m_conds{mc});
     end
 end
