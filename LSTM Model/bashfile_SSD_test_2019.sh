9742bb98f7ecc1f46e9fb7c04b227fffd9ef9e6d
simon.hofmann <shoffmann@cbs.mpg.de>
2019-03-08 10:00:21 +0100

    del files

--- LSTM Model/bashfile_SSD_test_2019.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# 2019 SSD Test Bashfile:
-python3 NeVRo.py --subject 36 --seed True --task regression --repet_scalar 2 --lstm_size 10 --fc_n_hidden 1 --component 1,3,5 --condition nomov
