0642ee0ed32b7288ba43480ff3c16bc0318f00f2
simon.hofmann <shoffmann@cbs.mpg.de>
2019-03-19 11:37:03 +0100

    add another gpu test

--- LSTM Model/gpu_test.py
+++ LSTM Model/gpu_test.py
@@ -4,6 +4,7 @@ Test GPU implementation
 """
 
 import tensorflow as tf
+from datetime import datetime
 
 print("GPU device is available:",
       tf.test.is_gpu_available())  # Returns True iff a gpu device of the requested kind is available.
@@ -21,3 +22,23 @@ print(sess.run(c))
 
 if 'gpu' not in compute_on:
     print("Can only be run on CPU")
+
+
+# Another test
+device_name = "/gpu:0" if tf.test.is_gpu_available() else "/cpu:0"
+
+for shape in [1500, 3000, 4500, 6000]:
+    with tf.device(device_name):
+        random_matrix = tf.random_uniform(shape=(shape, shape), minval=0, maxval=1)
+        dot_operation = tf.matmul(random_matrix, tf.transpose(random_matrix))
+        sum_operation = tf.reduce_sum(dot_operation)
+
+    startTime = datetime.now()
+    with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as session:
+            result = session.run(sum_operation)
+            print(result)
+
+    print("\n" * 3)
+    print("Shape:", (shape, shape), "Device:", device_name)
+    print("Time taken:", datetime.now() - startTime)
+    print("\n" * 3)
