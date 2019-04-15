63ff3b0198a7f473c234afc4992c949f0742ea57
Felix Klotzsche <klotzsche@engels.cbs.mpg.de>
2019-04-15 19:36:00 +0200

    add alpha helper; start 07_SSD in preprocessing

---
+++ Analysis/Preprocessing/EEG/NVR_07_SSD.m
@@ -0,0 +1,77 @@
+
+%% NVR_07_SSD
+%
+% Run SSD.
+
+% 
+% 
+% 
+
+% 2019: Alberto Mariola & Felix Klotzsche --- eioe
+
+
+function NVR_07_SSD(cropstyle, mov_cond, varargin)
+
+% check input:
+if nargin > 2
+    alphaPeakSource = varargin{1};
+else
+    alphaPeakSource = mov_cond;
+end
+
+%1.1 Set different paths:
+path_data = '../../../Data/';
+path_dataeeg =  [path_data 'EEG/'];
+path_in_eeg = [path_dataeeg '06_rejcomp/' mov_cond '/' cropstyle '/'];
+path_in_aPeaks = [path_dataeeg '07_SSD/'];
+
+% output paths:
+path_out_eeg = [path_dataeeg '07_SSD/' mov_cond '/' cropstyle '/'];
+if ~exist(path_out_eeg, 'dir'); mkdir(path_out_eeg); end
+
+%1.2 Get data files
+files_eeg = dir([path_in_eeg '*.set']);
+
+% Get the alpha peaks:
+
+alphaPeaks = load([path_in_aPeaks 'alphaPeaks.mat'], 'alphaPeaks');
+alphaPeaks = alphaPeaks.alphaPeaks; % now I would like to throw up.
+
+
+for isub = (1:length(files_eeg))
+
+    %1.3 Launch EEGLAB:
+    [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
+    
+    
+    % 1.4 Get subj name for getting the right event file later:
+    thissubject = files_eeg{isub};
+    thissubject = strsplit(thissubject, mov_cond);
+    thissubject = thissubject{1};
+    %thissubject = 'NVR_S06';
+    
+    
+    %1.5 Set filename:
+    filename = strcat(thissubject, mov_cond, '_PREP_', cropstyle, '_eventsaro_rejcomp');
+    filename = char(filename);
+
+    % 2.Import EEG data
+    [EEG, com] = pop_loadset([path_in_eeg, filename '.set']);
+    EEG = eegh(com,EEG);
+    
+    % Get the individual alpha peak:
+    alphaPeaks(
+    
+    [ALLEEG EEG] = pop_ssd(ALLEEG,EEG,CURRENTSET,...
+                           num2str(locs),...
+                           '2',...
+                           0,...
+                           '15',...
+                           1,...
+                           0,...
+                           0,...
+                           '',...
+                           []);
+
+end
+
