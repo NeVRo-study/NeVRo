b5cf9b1cedb4be449b4262ae3dcfecfb1a20a308
simon.hofmann <shoffmann@cbs.mpg.de>
2019-04-16 19:12:52 +0200

    move 1/f reg-out

--- Analysis/Preprocessing/EEG/NVR_07-1_1-f-regout.py
+++ Analysis/Preprocessing/EEG/NVR_07-1_1-f-regout.py
@@ -19,7 +19,7 @@ p2ssdnomov = p2ssd + "nomov/SBA/broadband/"
 # < o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >>
 
 
-sub = 14  # rnd subject
+sub = 2  # rnd subject
 
 # < o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >>
 new_ssd = False
@@ -102,6 +102,63 @@ ax2.set_title("plt.psd()")
 
 plt.show()
 
+# subplot per comps
+# for ch in range(1, sub_df.shape[1]):
+
+rpl = int(n_comp/2) if n_comp % 2 == 0 else int(n_comp/2) + 1
+
+
+opt = [0, 1]
+for o in opt:
+    freq_peak = []
+    min_max_Pxx_den = 99
+
+    figs = plt.figure(figsize=[14, 10])
+
+    for ch in range(n_comp):
+        axs = figs.add_subplot(rpl, 2, ch + 1)
+
+        f, Pxx_den = welch(x=sub_df[:, ch], fs=250.0, window="hann", nperseg=None, noverlap=None,
+                           nfft=None,
+                           detrend='constant', return_onesided=True, scaling='density', axis=-1,
+                           average='mean')
+
+        max_Pxx_den = max(Pxx_den)
+        freq_peak.append(f[np.where(Pxx_den == max_Pxx_den)])
+        min_max_Pxx_den = max_Pxx_den if max_Pxx_den < min_max_Pxx_den else min_max_Pxx_den
+
+        if o == 0:
+            axs.semilogy(f, Pxx_den)
+        else:
+            axs.plot(f, Pxx_den)
+        axs.set_xlabel('frequency [Hz]')
+        axs.set_ylabel('PSD [V**2/Hz]')
+
+        axs.vlines(f[np.where(Pxx_den == max_Pxx_den)], ymin=-0.01, ymax=min_max_Pxx_den, linestyles="dashed",
+                   alpha=.2)
+        xt = axs.get_xticks()
+        xt = np.append(xt, f[np.where(Pxx_den == max_Pxx_den)][0])
+        xtl = xt.tolist()
+        xtl[-1] = str(np.round(f[np.where(Pxx_den == max_Pxx_den)][0], 1))
+        axs.set_xticks(xt)
+        axs.set_xticklabels(xtl)
+
+        axs.set_title("SSD_comp_{}".format(ch+1))
+        axs.set_xlim([-1, 130])
+        if o == 1:
+            axs.set_ylim([0, 1])
+
+    plt.tight_layout()
+
+# # Alternative with plt.pds()
+# figs = plt.figure(figsize=[14, 10])
+# for ch in range(n_comp):
+#     axs = figs.add_subplot(rpl, 2, ch+1)
+#     axs.psd(x=sub_df[:, ch], Fs=250.)
+#     axs.set_title("SSD_comp_{}".format(ch+1))
+#     axs.set_xlim([-1, 130])
+# plt.tight_layout()
+
 # < o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >>
 
 # Detrend
@@ -139,33 +196,103 @@ plt.tight_layout()
 plt.show()
 
 # < o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >>
-fig4 = plt.figure()
-model = np.polyfit(f, np.log(Pxx_den), 1)
-predicted = np.polyval(model, f)
 
-model2 = np.polyfit(f, np.log(Pxx_den), 2)
-predicted2 = np.polyval(model2, f)
 
-model3 = np.polyfit(f, np.log(Pxx_den), 3)
-predicted3 = np.polyval(model3, f)
+for ch in range(sub_df.shape[1]):
 
-ax5 = plt.subplot(2, 1, 1)
-plt.plot(f, np.log(Pxx_den), label='data')
-plt.plot(predicted, alpha=.8, linestyle=":", label='poly_1/linear')
-plt.plot(predicted2, alpha=.8, linestyle=":", label='poly_2')
-plt.plot(predicted3, alpha=.8, linestyle=":", label='poly_3')
-ax5.set_title("Original")
+    fig4 = plt.figure()
+
+    f, Pxx_den = welch(x=sub_df[:, ch], fs=250.0, window="hann", nperseg=None, noverlap=None,
+                       nfft=None,
+                       detrend='constant', return_onesided=True, scaling='density', axis=-1,
+                       average='mean')
+
+    model = np.polyfit(f, np.log(Pxx_den), 1)
+    predicted = np.polyval(model, f)
+
+    model2 = np.polyfit(f, np.log(Pxx_den), 2)
+    predicted2 = np.polyval(model2, f)
+
+    model3 = np.polyfit(f, np.log(Pxx_den), 3)
+    predicted3 = np.polyval(model3, f)
+
+    np.polyfit(f, np.log(Pxx_den), 3)
+
+    ax5 = plt.subplot(2, 1, 1)
+    plt.plot(f, np.log(Pxx_den), label='data')
+    plt.plot(predicted, alpha=.8, linestyle=":", label='poly_1/linear')
+    plt.plot(predicted2, alpha=.8, linestyle=":", label='poly_2')
+    plt.plot(predicted3, alpha=.8, linestyle=":", label='poly_3')
+    ax5.set_title("Original SSD comp{}".format(ch+1))
+    plt.legend()
+    plt.tight_layout()
+
+    ax6 = plt.subplot(2, 1, 2)
+    plt.plot(f, np.log(Pxx_den), linestyle=":", label='data')
+    plt.plot(f, np.log(Pxx_den) - predicted, label='poly_1/linear')
+    plt.plot(f, np.log(Pxx_den) - predicted2, label='poly_2')
+    plt.plot(f, np.log(Pxx_den) - predicted3, label='poly_3')
+    # plt.plot(f, np.exp(np.log(Pxx_den)), label='exp')
+    ax6.set_title("Detrend")
+    plt.legend()
+    plt.tight_layout()
+
+    plt.show()
+
+
+# Smaller freq-window
+
+fig5 = plt.figure()
+f_small = f[f < 50]
+Pxx_den_small = Pxx_den[f<50]
+
+model3_small = np.polyfit(f_small, np.log(Pxx_den_small), 3)
+predicted3_small = np.polyval(model3_small, f_small)
+
+ax7 = plt.subplot(2, 1, 1)
+plt.plot(f_small, np.log(Pxx_den_small), label='data (f<50)')
+plt.plot(predicted3_small, alpha=.8, linestyle=":", label='poly_3')
+ax7.set_title("Original SSD comp{}".format(ch+1))
 plt.legend()
 plt.tight_layout()
 
-ax6 = plt.subplot(2, 1, 2)
-plt.plot(f, np.log(Pxx_den), linestyle=":", label='data')
-plt.plot(f, np.log(Pxx_den) - predicted, label='poly_1/linear')
-plt.plot(f, np.log(Pxx_den) - predicted2, label='poly_2')
-plt.plot(f, np.log(Pxx_den) - predicted3, label='poly_3')
-plt.plot(f, np.exp(np.log(Pxx_den)), label='exp')
-ax6.set_title("Detrend")
+ax8 = plt.subplot(2, 1, 2)
+plt.plot(f_small, np.log(Pxx_den_small), linestyle=":", label='data (f<50)')
+plt.plot(f_small, np.log(Pxx_den_small) - predicted3_small, label='poly_3')
+# plt.plot(f, np.exp(np.log(Pxx_den)), label='exp')
+ax8.set_title("Detrend")
 plt.legend()
 plt.tight_layout()
 
 plt.show()
+
+
+# Leave alpha-out freq-window
+fig6 = plt.figure()
+f_small_alphout = f_small[~((15 > f_small) & (f_small > 6))]
+Pxx_den_small_alphout = Pxx_den_small[~((15 > f_small) & (f_small > 6))]
+
+model3_small_alphout = np.polyfit(f_small_alphout, np.log(Pxx_den_small_alphout), deg=3)
+predicted3_small_alphout = np.polyval(model3_small_alphout, f_small)
+
+
+ax9 = plt.subplot(2, 1, 1)
+plt.plot(f_small, np.log(Pxx_den_small), label='data (f<50, alpha_out)')
+plt.plot(predicted3_small_alphout, alpha=.8, linestyle=":", label='poly_3')
+ax9.set_title("Original SSD comp{}".format(ch+1))
+plt.legend()
+plt.tight_layout()
+
+ax10 = plt.subplot(2, 1, 2)
+plt.plot(f_small, np.log(Pxx_den_small), linestyle=":", label='data (f<50, alpha_out)')
+plt.plot(f_small, np.log(Pxx_den_small) - predicted3_small_alphout, label='poly_3')
+# plt.plot(f, np.exp(np.log(Pxx_den)), label='exp')
+ax10.set_title("Detrend")
+plt.legend()
+plt.tight_layout()
+
+plt.show()
+
+plt.figure()
+plt.plot(predicted3_small)
+plt.plot(predicted3_small_alphout)
