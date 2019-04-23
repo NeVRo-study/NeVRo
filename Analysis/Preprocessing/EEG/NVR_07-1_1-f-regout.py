4f9461f477a106710b1b0a549a70368852ab2fdc
simon.hofmann <shoffmann@cbs.mpg.de>
2019-04-23 18:03:48 +0200

    1/f reg-out: alpha-out + window polyfit

--- Analysis/Preprocessing/EEG/NVR_07-1_1-f-regout.py
+++ Analysis/Preprocessing/EEG/NVR_07-1_1-f-regout.py
@@ -16,13 +16,18 @@ p2ssd = path_data + "EEG/07_SSD/"
 # < o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >>
 
 # Set hyperparameters
-sanity_check = False  # plot additional infos (see below)
-tight_range = True  # Freq-range 0-50 Hz
+sanity_check = True  # plot additional infos (see below)
+tight_range = True  # Freq-range 0-max_range Hz
+max_range = 50  # 20 / 50 Hz
+alpha_out_window = True  # False: only alpha out poly3-fit # TODO adapt
+
+save_plots = False
 subjects = np.arange(1, 45+1)  # ALL
 # subjects = np.arange(1, 20+1)  # subjects = np.arange(21, 45+1)  # subsets
 # subjects = np.array([6, 15, 18, 21, 22, 26, 27, 31, 35])  # subset: check selections
 # subjects = np.array([7 , 14, 15, 21, 25])  # subset: check alpha peak info
 # subjects = np.array([6])  # subset: single subject
+subjects = np.array([11])  # subset: single subject
 condition = "nomov"
 
 # < o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >>
@@ -80,8 +85,8 @@ for sub in subjects:
                            average='mean')
 
         if tight_range:
-            Pxx_den = Pxx_den[f <= 50]
-            f = f[f <= 50]
+            Pxx_den = Pxx_den[f <= max_range]
+            f = f[f <= max_range]
 
         ax.semilogy(f, Pxx_den)
         plt.xlabel('frequency [Hz]')
@@ -118,15 +123,16 @@ for sub in subjects:
 
     plt.tight_layout()
     plt.show()
-    plt.savefig(fname=p2ssd + "{}/selection_plots/{}_SSD_powerspec.png".format(condition, s(sub)))
-    plt.close()
+    if save_plots:
+        plt.savefig(fname=p2ssd + "{}/selection_plots/{}_SSD_powerspec.png".format(condition, s(sub)))
+        plt.close()
 
 # < o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >>
 
     # # Detrend
     # Subplot per component
 
-    # Define Grid-size
+    # Define figure grid-size: with rpl x cpl cells
     rpl = 1
     cpl = 1
     while (rpl*cpl) < n_comp:
@@ -140,7 +146,7 @@ for sub in subjects:
 
         for ch in range(n_comp):
 
-            axs = figs2.add_subplot(rpl, 2, ch + 1)
+            axs = figs2.add_subplot(rpl, cpl, ch + 1)
 
             f, Pxx_den = welch(x=sub_df[:, ch], fs=250.0, window="hann", nperseg=None, noverlap=None,
                                nfft=None,
@@ -148,8 +154,8 @@ for sub in subjects:
                                average='mean')
 
             if tight_range:
-                Pxx_den = Pxx_den[f <= 50]
-                f = f[f <= 50]
+                Pxx_den = Pxx_den[f <= max_range]
+                f = f[f <= max_range]
 
             Pxx_den_apeak = np.log(Pxx_den)[np.argmin(np.abs(f - sub_apeak))]
 
@@ -171,7 +177,7 @@ for sub in subjects:
             # axs.plot(predicted, alpha=.8, linestyle=":", c="g", label='poly_1/linear')
             axs.plot(predicted2, alpha=.8, linestyle=":", c="y", label='poly_2')
             axs.plot(predicted3, alpha=.8, linestyle=":", c="m", label='poly_3')
-            axs.set_title("Detrend SSD comp{}".format(ch+1))
+            axs.set_title("S{} | {} | Detrend SSD comp{}".format(s(sub), condition, ch+1))
 
             # axs.plot(f, np.log(Pxx_den) - predicted, c="g", label='poly_1/linear')
             axs.plot(f, np.log(Pxx_den) - predicted2, c="y", label='detrend/poly_2')
@@ -187,25 +193,29 @@ for sub in subjects:
             if ch == 0:
                 axs.legend(loc='upper right')
             plt.tight_layout()
-            plt.show()
+        plt.show()
+        # plt.savefig(fname=p2ssd + "{}/selection_plots/{}_SSD_Detrend_polies.png".format(condition,
+        #                                                                                 s(sub)))
+        if save_plots:
+            plt.close()
 
-    # Smaller freq-window (0-50Hz)  + Leave alpha-out
+    # Smaller freq-window (0-max_range Hz)  + Leave alpha-out
 
     if sanity_check:
         figs3 = plt.figure(figsize=[14, 10])
 
         for ch in range(n_comp):
 
-            axs = figs3.add_subplot(rpl, 2, ch + 1)
+            axs = figs3.add_subplot(rpl, cpl, ch + 1)
 
             f, Pxx_den = welch(x=sub_df[:, ch], fs=250.0, window="hann", nperseg=None, noverlap=None,
                                nfft=None,
                                detrend='constant', return_onesided=True, scaling='density', axis=-1,
                                average='mean')
 
-            # Freq.-Range (0-50Hz)
-            f_small = f[f <= 50]
-            Pxx_den_small = Pxx_den[f <= 50]
+            # Freq.-Range (0-max_range Hz)
+            f_small = f[f <= max_range]
+            Pxx_den_small = Pxx_den[f <= max_range]
 
             # Leave alpha out
             f_small_alphout = f_small[~((sub_apeak+4 > f_small) & (f_small > sub_apeak-4))]
@@ -219,23 +229,53 @@ for sub in subjects:
             model3_small_alphout = np.polyfit(f_small_alphout, np.log(Pxx_den_small_alphout), deg=3)
             predicted3_small_alphout = np.polyval(model3_small_alphout, f_small)
 
-            plt.plot(f_small, np.log(Pxx_den_small), linestyle="-.", label='data (f<=50)')
-            plt.plot(predicted3_small, alpha=.8, linestyle=":", c="m", label='poly3')
+            # Plot
+            plt.plot(f_small, np.log(Pxx_den_small), linestyle="-.",
+                     label='data (f<={})'.format(max_range))
+            # plt.plot(predicted3_small, alpha=.8, linestyle=":", c="m", label='poly3')
             plt.plot(predicted3_small_alphout, alpha=.8, c="g", linestyle=":", label='poly3_alpha-out')
-
-            plt.plot(f_small, np.log(Pxx_den_small) - predicted3_small, c="m",
-                     label='detrend/poly_3')
+            # plt.plot(f_small, np.log(Pxx_den_small) - predicted3_small, c="m",
+            #          label='detrend/poly_3')
             plt.plot(f_small, np.log(Pxx_den_small) - predicted3_small_alphout, c="g",
                      label='detrend/poly3_alpha-out')
+
+            # Leave alpha out + window around alpha
+            if alpha_out_window:
+                f_small_alphout_window = f_small_alphout[(f_small_alphout < sub_apeak+10) &
+                                                         (f_small_alphout > sub_apeak-6)]
+                Pxx_den_small_alphout_window = Pxx_den_small_alphout[(f_small_alphout < sub_apeak+10) &
+                                                                     (f_small_alphout > sub_apeak-6)]
+
+                # Fit polynomial(3) to alpha out + window data
+                model4_small_alphout_window = np.polyfit(f_small_alphout_window,
+                                                         np.log(Pxx_den_small_alphout_window),
+                                                         deg=3)
+                # define only for f<20
+                predicted4_small_alphout_window = np.polyval(model4_small_alphout_window,
+                                                             f_small[f_small < 20])
+
+                # Plot
+                plt.plot(predicted4_small_alphout_window, alpha=.8, c="y", linestyle=":",
+                         label='poly3_a-out-window')
+                plt.plot(f_small[f_small < 20],
+                         np.log(Pxx_den_small)[f_small < 20] - predicted4_small_alphout_window, c="y",
+                         label='detrend/poly3_a-out-window')
+
             axs.vlines(sub_apeak,
                        ymin=axs.get_ylim()[0],
                        ymax=np.log(Pxx_den_small)[np.argmin(np.abs(f_small - sub_apeak))],
                        linestyles="dashed", alpha=0.2)  # ymax=np.polyval(model3_small, sub_apeak)
-            axs.set_title("Detrend SSD comp{}".format(ch+1))
+
+            axs.set_title("S{} | {} | Detrend SSD comp{}".format(s(sub), condition, ch + 1))
+
             if ch == 0:
                 plt.legend(loc='upper right')
             plt.tight_layout()
-            plt.show()
+
+        plt.show()
+        if save_plots:
+            plt.savefig(fname=p2ssd + "{}/selection_plots/{}_SSD_Detrend.png".format(condition, s(sub)))
+            plt.close()
 
 # < o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >>
 
@@ -256,15 +296,15 @@ for sub in subjects:
                            average='mean')
 
         if tight_range:
-            # Freq.-Range (0-50Hz)
-            Pxx_den = Pxx_den[f <= 50]
-            f = f[f <= 50]
+            # Freq.-Range (0-max_range Hz)
+            Pxx_den = Pxx_den[f <= max_range]
+            f = f[f <= max_range]
 
         # Leave alpha out
         f_alphout = f[~((sub_apeak + 4 > f) & (f > sub_apeak - 4))]
         Pxx_den_alphout = Pxx_den[~((sub_apeak + 4 > f) & (f > sub_apeak - 4))]
 
-        # Fit polynomial(3) to alpha out data
+        # Fit polynomial(3) to alpha-out data
         model3_alphout = np.polyfit(f_alphout, np.log(Pxx_den_alphout), deg=3)
         predicted3_alphout = np.polyval(model3_alphout, f)  # predict on whole! freq-range
 
@@ -273,27 +313,37 @@ for sub in subjects:
         # Define area around given alpha-peak
         f_apeak = f[((f > sub_apeak - 4) & (sub_apeak + 4 > f))]
         log_Pxx_den_apeak = log_Pxx_den_detrend[((f > sub_apeak - 4) & (sub_apeak + 4 > f))]
+
         # Define adjacent area
         log_Pxx_den_apeak_flank_links = log_Pxx_den_detrend[(f <= sub_apeak - 4)][-2:]
         log_Pxx_den_apeak_flank_right = log_Pxx_den_detrend[(f >= sub_apeak + 4)][:2]
-        # plt.plot(f, log_Pxx_den_detrend)
-        # plt.plot(f[((f > sub_apeak - 4) & (sub_apeak + 4 > f))], log_Pxx_den_apeak)
-        # plt.plot(f[(f <= sub_apeak - 4)][-2:], log_Pxx_den_apeak_flank_links)
-        # plt.plot(f[(f >= sub_apeak + 4)][:2], log_Pxx_den_apeak_flank_right)
+
+        if alpha_out_window:
+            # Leave alpha out + window around alpha
+            f_aout_window = f_alphout[(f_alphout < sub_apeak + 10) & (f_alphout > sub_apeak - 6)]
+            Pxx_den_aout_window = Pxx_den_alphout[(f_alphout < sub_apeak + 10) &
+                                                  (f_alphout > sub_apeak - 6)]
+
+            # Fit polynomial(3) to alpha out + window data
+            # Define only for f<20
+            model4_small_aout_window = np.polyfit(f_aout_window, np.log(Pxx_den_aout_window), deg=3)
+            predicted4_aout_window = np.polyval(model4_small_aout_window, f[f < 20])
+
+            log_Pxx_den_detrend_awind = np.log(Pxx_den[f < 20]) - predicted4_aout_window
+
+            log_Pxx_den_apeak_awind = log_Pxx_den_detrend_awind[((f[f < 20] > sub_apeak - 4) &
+                                                                 (sub_apeak + 4 > f[f < 20]))]
+
+            log_Pxx_den_apeak_awind_flank_links = log_Pxx_den_detrend_awind[(
+                    f[f < 20] <= sub_apeak - 4)][-2:]
+            log_Pxx_den_apeak_awind_flank_right = log_Pxx_den_detrend_awind[(
+                    f[f < 20] >= sub_apeak + 4)][:2]
 
         # # # Select
-        # # Criterion option 1): alpha-peak not the smallest above zero-line
-        # log_Pxx_den_detrend_above_zero = log_Pxx_den_detrend[log_Pxx_den_detrend > 0]
-        # f_above_zero = f[log_Pxx_den_detrend > 0]
-        # log_Pxx_den_detrend_above_zero /= max(log_Pxx_den_detrend_above_zero)  # normalize
-        # axs.plot(f_above_zero, log_Pxx_den_detrend_above_zero)
-        # min(log_Pxx_den_detrend_above_zero)
-        # TODO ... continue
-
-        # # Criterion option 2): alpha-peak above zero-line + small error term
-        error_term = 0.01  # TODO can be defined more systematically
+        # # Criterion: alpha-peak above zero-line + small error term
+        error_term = 0.01  # TODO Could be defined more systematically
         if np.any(log_Pxx_den_apeak > 0 + error_term):
-            # # Additional Criterion 2.2): peak in area > adjacent areas
+            # # Additional criterion: peak in area > adjacent areas
             if np.max(log_Pxx_den_apeak) > np.mean(log_Pxx_den_apeak_flank_links) and \
                     np.max(log_Pxx_den_apeak) > np.mean(log_Pxx_den_apeak_flank_right):
                 # write ch (component) as selected
@@ -303,14 +353,31 @@ for sub in subjects:
             # Throw ch (component) out
             selected = False
 
-        axs.plot(f, log_Pxx_den_detrend)
+        # Alternatively: Selection on Alpha-out + window
+        # TODO adapt, incl. plots
+        if alpha_out_window:
+            selected = False  # Init
+            if np.any(log_Pxx_den_apeak_awind > 0 + error_term):
+                # # Additional Criterion 2.2): peak in area > adjacent areas
+                if np.max(log_Pxx_den_apeak_awind) > np.mean(log_Pxx_den_apeak_awind_flank_links) and \
+                        np.max(log_Pxx_den_apeak_awind) > np.mean(log_Pxx_den_apeak_awind_flank_right):
+                    # write ch (component) as selected
+                    selected = True
+                    selected_comps.append(ch+1)  # range(1, ...)
+
+            axs.plot(f[f < 20], predicted4_aout_window, c="m", linestyle=":", alpha=.5)  # polyfit
+
+
+        axs.plot(f, np.log(Pxx_den), c="b", alpha=.5)  # Original
+        axs.plot(f, predicted3_alphout, c="b", linestyle=":", alpha=.5)  # polyfit
+        axs.plot(f, log_Pxx_den_detrend, c="b")  # Detrend
         axs.plot(f_apeak, log_Pxx_den_apeak, c="g" if selected else "r")
         axs.set_title("{} | {} | SSD comp{}".format(s(sub), condition, ch+1))
         axs.vlines(sub_apeak,
                    ymin=min(log_Pxx_den_detrend),
                    ymax=log_Pxx_den_detrend[np.argmin(np.abs(f - sub_apeak))],
                    linestyles="dashed", alpha=.2)
-        plt.hlines(y=0, xmin=0, xmax=50, alpha=.4, linestyles=":")  # Zero-line
+        plt.hlines(y=0, xmin=0, xmax=max_range, alpha=.4, linestyles=":")  # Zero-line
 
         log_Pxx_den_apeak_stand = log_Pxx_den_apeak - np.linspace(log_Pxx_den_apeak[0],
                                                                   log_Pxx_den_apeak[-1],
@@ -322,8 +389,10 @@ for sub in subjects:
         # axs.fill_between(f_apeak, np.array(log_Pxx_den_apeak_stand+.5), alpha=.2)
 
     plt.tight_layout()
-    plt.savefig(fname=p2ssd + "{}/selection_plots/{}_SSD_selection.png".format(condition, s(sub)))
-    plt.close()
+    plt.show()
+    if save_plots:
+        plt.savefig(fname=p2ssd + "{}/selection_plots/{}_SSD_selection.png".format(condition, s(sub)))
+        plt.close()
 
     # Write selected SSD components in table
     tab_select_ssd[np.where(tab_select_ssd[:, 0] == str(sub)), 1] = ",".join(
