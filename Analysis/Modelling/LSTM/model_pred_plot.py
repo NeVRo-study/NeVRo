4c55f2bc7a45bfe917e7c7dd29a89044465c85e6
SHEscher <zimon@gmx.com>
2020-05-15 14:54:46 +0200

    upd across model plot

--- Analysis/Modelling/LSTM/model_pred_plot.py
+++ Analysis/Modelling/LSTM/model_pred_plot.py
@@ -15,7 +15,8 @@ from scipy.signal import savgol_filter
 
 save_plots = True
 
-task = "classification"
+lw = 1  # line-width
+fs = 14
 
 # Set Paths
 wdic_results = f"../../../Results/"
@@ -43,7 +44,8 @@ for cond in ["nomov", "mov"]:  # Conditions
                 wdic = "./processed"
                 wdic_sub = wdic + f"/{cond}/{s(subject)}/already_plotted/"
                 # Get path specs from table of model performance per subject
-                wdic_pathspecs = wdic+f"/Random_Search_Tables/{cond}/1_narrow_search/{task}/per_subject/"
+                wdic_pathspecs = wdic+f"/Random_Search_Tables/{cond}/1_narrow_search/" \
+                                      f"classification/per_subject/"
                 perform_tab = pd.read_csv(wdic_pathspecs+f"AllSub_Narrow_Search_Table_{cond}_BiCl.csv",
                                           sep=";")
                 path_specificity = perform_tab.loc[perform_tab["subject"] ==
@@ -72,25 +74,21 @@ for cond in ["nomov", "mov"]:  # Conditions
                                                       mat_idx=dub_shuffle_order_matrix)
                     del dub_shuffle_order_matrix
 
-                # Number of Folds
-                s_fold = int(len(pred_matrix[:, 0]) / 2)
-
                 # Load full rating vector
                 whole_rating = load_rating_files(subjects=subject,
                                                  condition=cond,
                                                  bins=True)[str(subject)]["SBA"][cond]
 
-                real_rating = None  # init
-                only_entries_rating = None  # init
-                lower_tert_bound = None  # init
-                upper_tert_bound = None  # init
-
                 whole_rating[np.where(whole_rating == 0)] = np.nan
-                real_rating = normalization(array=load_rating_files(
-                    subjects=subject,
-                    condition=cond,
-                    sba="SBA",
-                    bins=False)[str(subject)]["SBA"][cond], lower_bound=-1, upper_bound=1)  # range -1, 1
+
+                subject_rating = load_rating_files(subjects=subject,
+                                                   condition=cond, sba="SBA",
+                                                   bins=False)[str(subject)]["SBA"][cond]
+
+                # Get ratings
+                real_rating = normalization(subject_rating, -1, 1)
+                # real_rating = z_score(subject_rating)
+                # real_rating /= np.max(np.abs(real_rating))  # bound abs(max-val) to 1
 
                 tertile = int(len(real_rating) / 3)
                 lower_tert_bound = np.sort(real_rating)[tertile]  # np.percentile(a=real_rating, q=33.33)
@@ -100,12 +98,9 @@ for cond in ["nomov", "mov"]:  # Conditions
                 only_entries_rating = real_rating.copy()
                 only_entries_rating[np.where(no_entries)] = np.nan
 
-                whole_rating_shift = copy.copy(whole_rating)
-                for idx, ele in enumerate(whole_rating):
-                    if ele == 1.:
-                        whole_rating_shift[idx] = ele + .1
-                    elif ele == -1.:
-                        whole_rating_shift[idx] = ele - .1
+                whole_rating_shift = copy.copy(whole_rating)  # for plotting
+                for _bin in [-1, 1]:
+                    whole_rating_shift[whole_rating_shift == _bin] = _bin*(np.max(_bin*real_rating) + .1)
 
             # Get concatenated validation predictions for given subject and model
             model_prediction = pd.read_csv(
@@ -124,31 +119,31 @@ for cond in ["nomov", "mov"]:  # Conditions
                     sba="SBA",
                     bins=False)[str(subject)]["SBA"][cond])
 
-                # < o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >
+        # < o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >
 
             # # # Plotting
             if midx == 0:
-                fig = plt.figure(f"Predictions of all models | {s(subject)} | {cond} | {task} | 1Hz ",
+                fig = plt.figure(f"Predictions of all models | {s(subject)} | {cond} | 1Hz ",
                                  figsize=(10, 12))
 
             ax = fig.add_subplot(3, 1, midx+1)
 
-            lw = 0.5  # line-width
-
-            # Plot val predictions
-            plt.plot(model_prediction, marker="o", markerfacecolor="None", ms=2, c="aquamarine",
-                     linewidth=2 * lw, label="concatenated val-prediction")
-
             # Ratings
-            plt.plot(real_rating, color="darkgrey", alpha=.5)
+            plt.plot(real_rating, color="darkgrey", alpha=.5, lw=lw*2)
+
             # midline and tertile borders
             plt.hlines(y=0, xmin=0, xmax=pred_matrix.shape[1], colors="darkgrey", lw=lw, alpha=.8)
 
             if model != "SPoC":
 
-                plt.plot(only_entries_rating, color="teal", alpha=.3, label="ground-truth rating")
+                # Plot val predictions
+                plt.plot(model_prediction, marker="o", markerfacecolor="None", ms=2, c="aquamarine",
+                         lw=2 * lw, label="concatenated val-prediction")
+
+                plt.plot(only_entries_rating, color="teal", alpha=.3, lw=lw * 2,
+                         label="ground-truth rating")
                 plt.plot(whole_rating_shift, ls="None", marker="s", markerfacecolor="None", ms=3,
-                         color="black", label="arousal classes: low=-1 | high=1")
+                         color="black", label="arousal classes: low & high")
 
                 plt.hlines(y=lower_tert_bound, xmin=0, xmax=pred_matrix.shape[1], linestyle="dashed",
                            colors="darkgrey", lw=lw, alpha=.8)
@@ -175,40 +170,51 @@ for cond in ["nomov", "mov"]:  # Conditions
 
                 for i in range(correct_class.shape[0]):
                     corr_col = "lime" if correct_class[i] == 1 else "orangered"
-                    # wr = whole_rating[i]
                     wr = whole_rating_shift[i]
                     # plt.vlines(i, ymin=wr, ymax=model_prediction[i], colors=corr_col, alpha=.5,
                     #            lw=lw/1.5)
                     if not np.isnan(model_prediction[i]):
                         # set a point at the end of line
-                        plt.plot(i, np.sign(wr) * (np.abs(wr) + .01) if correct_class[i] == 1 else wr,
-                                 marker="o", color=corr_col, alpha=.5, ms=2)
+                        plt.plot(i, wr, marker="o", color=corr_col, alpha=.5, ms=2)
 
                 # Adapt y-axis ticks
-                ax.set_yticks([-1, 0, 1])
-                ax.set_yticklabels(["low", "0", "high"])
-                plt.ylim(-1.2, 1.6 if midx == 0 else 1.2)
+                ax.set_yticks([np.nanmin(whole_rating_shift),
+                               0,
+                               np.nanmax(whole_rating_shift)])
+                ax.set_yticklabels(["low",
+                                    "db",  # decision boundary
+                                    "high"], fontdict={"fontsize": fs})
+                plt.ylim(np.nanmin(whole_rating_shift) - .1,
+                         np.nanmax(whole_rating_shift) + (.4 if midx == 0 else .1))
 
             else:
+
+                plt.plot(model_prediction, marker="o", markerfacecolor="None", ms=2, c="aquamarine",
+                         lw=2 * lw, label=r"$-\sqrt{z_{est}}$")
+
                 # Adapt y-axis ticks
-                ax.set_yticks([0])
+                ax.set_yticks([np.nanmedian(real_rating)])
+                ax.set_yticklabels(["0"], fontdict={"fontsize": fs})
                 # Set x-label
-                plt.xlabel("time(s)")
+                plt.xlabel("time(s)", fontdict={"fontsize": fs})
 
             # Set title
             if model != "SPoC":  # LSTM, CSP:
-                plt.title(label=f"{model} | concatenated predictions on validation sets | "
-                                f"mean {task}-accuracy={mean_acc:.3f}")
+                plt.title(label=f"{model} | " + r"$accuracy_{mean}=$" + f"{mean_acc:.3f}",
+                          fontdict={"fontsize": fs + 2})
+                # | concatenated predictions on validation sets |
             else:  # for SPoC:
                 r = float(result_tab.loc[f"NVR_{s(subject)}"][model.upper()+"_CORR"])
-                plt.title(label=f"{model} | " + r'$-\sqrt{z_{est}}$' + f" | max-correlation | r={r:.3f}")
+                plt.title(label=f"{model} | " + r"$r_{max}=$" + f"{r:.3f}",
+                          fontdict={"fontsize": fs+2})
+                plt.legend(fontsize=fs - 1)
 
             # adjust size, add legend
             plt.xlim(0, len(whole_rating))
             # plt.ylim(-1.2, 1.6 if midx == 0 else 1.2)
             if midx == 0:
                 plt.legend(bbox_to_anchor=(0., 0.90, 1., .102), loc=1, ncol=4, mode="expand",
-                           borderaxespad=0.)
+                           borderaxespad=0., fontsize=fs-1)
             plt.tight_layout(pad=2)
 
         # # Show & save plot
