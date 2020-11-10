a9e7baeead378e04729e93efcef8447c92812bc4
Alberto <alberto.mariola@icloud.com>
2020-11-10 02:17:12 +0000

    Updated SPoC and SPoC_Group

--- Analysis/Modelling/SPoC/NVR_SPoC.m
+++ Analysis/Modelling/SPoC/NVR_SPoC.m
@@ -1,23 +1,25 @@
 %% NeVRo_SPoC
-%This script computes SPoC between the alpha-band filtered components (8-12 Hz) and
-%the continous ratings of the participants (z).
+
+% This script computes SPoC between the SSD alpha-band filtered components and
+% the continous arousal ratings of participants (z).
 
 %% Clean previous mess
 
 clc
 clear all
-%close all
 
 %% Set paths
 
 % Add general matlab scripts and eeglab plugins
-addpath(genpath('/Users/Alberto/Documents/MATLAB/'));
+addpath(genpath('/Users/Alberto/MATLAB/'));
 
-% Open eeglab
+% Open EEGLAB
 [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
 
 % Paths
-master_path = '/Users/Alberto/Documents/PhD/PhD_Side/NeVRo/';
+master_path = '../../../../NeVRo/';
+addpath(master_path);
+
 ssd_path = [master_path 'Data/EEG/07_SSD/'];
 rat_path = [master_path 'Data/ratings/continuous/not_z_scored/']; 
 spoc_path = [master_path 'Data/EEG/08.1_SPOC/']; 
@@ -26,11 +28,14 @@ results_path = [master_path 'Results/EEG/SPOC/'];
 % Folders
 cond = {'nomov','mov'};
 
+% Set Seed for reproducibility
+rng(11); 
+
 %% Load Data
 
-for fold = 1:length(cond)
+for folder = 1:length(cond)
      
-rawDataFiles = dir([ssd_path cond{fold} '/SBA/narrowband/*.set']);  %we specifcally use SBA data
+rawDataFiles = dir([ssd_path cond{folder} '/SBA/narrowband/*.set']);  %we specifcally use SBA data
 
 % NB: for the mov condition, S09, S24 and S27 are problematic because none of
 % their SSD components passed our selection criteria 
@@ -51,14 +56,14 @@ loadName = rawDataFiles(isub).name;
 fileName = loadName(1:7);
 
 % Load EEG file
-[EEG,com]= pop_loadset([ssd_path cond{fold} '/SBA/narrowband/' loadName]);
+[EEG,com]= pop_loadset([ssd_path cond{folder} '/SBA/narrowband/' loadName]);
 EEG.setname=fileName;
 [ALLEEG,EEG,CURRENTSET] = eeg_store(ALLEEG, EEG);
 
 %% Choose the correct ratings and the selected SSD components to be extracted:
 
 % SSD Selected Components file
-ssd_tab = readtable([ssd_path cond{fold} '/SSD_selected_components_' cond{fold} '.csv']); %import the selected components table
+ssd_tab = readtable([ssd_path cond{folder} '/SSD_selected_components_' cond{folder} '.csv']); %import the selected components table
 %ssd_sel =  str2num(cell2num(ssd_tab{str2num(fileName(6:end)),2})); all together
     
 sel_sub = ssd_tab(str2num(fileName(6:end)),2); %select the correspondent participant's row
@@ -108,81 +113,77 @@ for i=1:270
     
 end 
 
-% 1. First approach: SPoC via EEGLAB (currently not in use)
-% Import events on the EEG file to allow spoc (currently using 1,2,3 -
-% Felix's CSP events)
-%[EEG, eventnumbers] = pop_importevent(EEG, 'append','no','event',epochs,'fields',{'latency','type'},'skipline',0,'timeunit',1,'align',1,'optimalign','on');
-
-% Run SPOC(need to adapt the epoch size):
-%EEG = pop_spoc(EEG,['z.mat'],'500','-0.5 0.496'); %SBA
-
-% 2. Second (actual) approach: "raw" code
-%Create 1 second epochs
+% Create 1 second epochs
 EEG = pop_epoch( EEG, {'1','2','3'}, [-0.5 0.5], 'newname', EEG.setname, 'epochinfo', 'yes');
 
-% 2.Original spoc function 
+% Transform data in the right format for SPoC's original function 
 X = permute(EEG.data, [2, 1, 3]);
 
 % Define parameters of X that we will need for further processing
 Te = length(X(:,:,1));  % number of samples per epoch
 Nx = length(X(1,:,1));  % number of sensors
 
+% Apply SPoC
 [W, A, lambda_values, p_values_lambda, Cxx, Cxxz, Cxxe] = spoc(X, z, 'n_bootstrapping_iterations',500);
 
-%%  Extract the best component(s) for each participant (spoc)
+%% OLD: find the maximum absolute lambda value
 
-%spoc_res = [lambda_values p_values_lambda' A' R'];
-spoc_res = [lambda_values p_values_lambda' A', W'];
-[spoc_res, index] = sortrows(spoc_res, 1, 'ascend');
+% Alternative if you
+%[M I] = max(abs(lambda_values));
 
 %% Compute z_est (the estimated time course of the behavioural data - the univariate variable z - with the best set of weights W)
+% AKA: projecting the data onto the best (most negative/last in our case) component
 
 s_est = zeros(Te, length(z));
 
 for k=1:length(z)
-    s_est(:,k) = squeeze(X(:,:,k)) * W(:,index(1));
+    s_est(:,k) = squeeze(X(:,:,k)) * W(:,end); %end to automatically take the last one ("most negative" lambda)
 end
 
 p_est = var(s_est);
 
 %% Compute correlation between z_est and z (in this case there's no training/test separation - might be overfitted)
 
-r_tot = corrcoef(p_est', z');
-r = r_tot(1,2:end);
+% Spearman
+r_tot  = corr(p_est', z', 'type', 'Spearman');
+
+% Pearson
+%r_tot = corrcoef(p_est', z');
+%r = r_tot(1,2:end);
 
 %% Save Spatial Patterns
-dlmwrite([spoc_path cond{fold} '/SBA/' fileName '_A.csv'], A); % save spatial patterns
-save([spoc_path cond{fold} '/SBA/' fileName '_A.mat'], 'A'); %spatial patterns
 
-%dlmwrite([spoc_path cond{fold} '/SBA/' fileName '_A.csv'],comp_tc); %time course
+dlmwrite([spoc_path cond{folder} '/SBA/' fileName '_A.csv'], A); % save spatial patterns
+save([spoc_path cond{folder} '/SBA/' fileName '_A.mat'], 'A'); %spatial patterns
+
+%dlmwrite([spoc_path cond{folder} '/SBA/' fileName '_A.csv'],comp_tc); %time course
 
 %% Add everything to the general SPOC_results structure (one per condition - only considering the best SPOC component)
 
 SPOC_results.chanlocs = EEG.chanlocs;
 SPOC_results.results(isub).participant = fileName;
 SPOC_results.results(isub).preprocessing = EEG.etc;
-%SPOC_results.results(isub).SPOC_model.data = X;
 SPOC_results.results(isub).SPOC_model.Cov = Cxx ;
 SPOC_results.results(isub).SPOC_model.Covz = Cxxz;
 SPOC_results.results(isub).SPOC_model.Covtr = Cxxe;
 SPOC_results.results(isub).SPOC_model.z_original = z;
 SPOC_results.results(isub).SPOC_model.z = (z-mean(z(:)))./std(z(:));
 SPOC_results.results(isub).SPOC_model.z_est = p_est;
-SPOC_results.results(isub).SPOC_model.corr = r;
+SPOC_results.results(isub).SPOC_model.corr = r_tot;
 SPOC_results.results(isub).SPOC_model.lambda = lambda_values;
 SPOC_results.results(isub).SPOC_model.p_values_lambda = p_values_lambda;
 SPOC_results.results(isub).stats = p_values_lambda;
-SPOC_results.results(isub).weights.SPOC_A = A(:,index(1));
-SPOC_results.results(isub).weights.SPOC_W = W(:,index(1));
+SPOC_results.results(isub).weights.SPOC_A = A(:,end);
+SPOC_results.results(isub).weights.SPOC_W = W(:,end);
 SPOC_results.results(isub).weights.SSD_W_sel = W_ssd;
 SPOC_results.results(isub).weights.SSD_A_sel = A_ssd;
 
 %% Update summary structure
 
 struct_summ(isub).ID = fileName;
-struct_summ(isub).lambda = spoc_res(1,1);
-struct_summ(isub).pval = spoc_res(1,2);
-struct_summ(isub).r = r;
+struct_summ(isub).lambda = lambda_values(end);
+struct_summ(isub).pval =  p_values_lambda(end);
+struct_summ(isub).r = r_tot;
 
 % Update z_est structure
 struct_zest(isub).ID = fileName;
@@ -192,32 +193,22 @@ struct_zest(isub).z_est = p_est;
 struct_z(isub).ID = fileName;
 struct_z(isub).z =(z-mean(z(:)))./std(z(:));
 
-
 % Remove empty fields
 struct_z = struct_z(~cellfun(@isempty,{struct_z.ID}));
 struct_zest = struct_zest(~cellfun(@isempty,{struct_zest.ID}));
 struct_summ = struct_summ(~cellfun(@isempty,{struct_summ.ID}));
 
-%% Group-level stats
-
-% Compute a one-sample, one-sided t.test to assess whether the mean
-% correlation value between z and z_est is sig. lower than 0 (aka
-% negative - coherently with our hypothesis). 
-
-[stats.h,stats.p,stats.ci,stats.stats] = ttest([struct_summ.r],0,'Tail','left')
-
-% Save struct
-save([spoc_path cond{fold} '/SBA/summaries/SPOC_groupstats_'  cond{fold} '.mat'], 'stats');
-
-%% Plotting
+%% OPTIONAL: Plotting
+% Show spatial patterns of each component and highlight info (p-value,
+% lambda, corr)
 
 %h(isub) = subplot_tight(6,6,isub, 0.06)
 h(isub) = subplot(6,6,isub)
- if spoc_res(1,2) <0.05
-     title({[fileName] ; ['p: ' num2str(spoc_res(1,2)) ' - l: ' num2str(spoc_res(1,1)) ' - r: ' num2str(r)]},'Color','r');
+ if p_values_lambda(end) <0.05
+     title({[fileName] ; ['p: ' num2str(p_values_lambda(end)) ' - l: ' num2str(lambda_values(end)) ' - r: ' num2str(r_tot)]},'Color','r');
      %xlabel(['p: ' num2str(spoc_res(1,2))], 'Color','r')
  else 
-     title({[fileName] ; ['p: ' num2str(spoc_res(1,2)) ' - l: ' num2str(spoc_res(1,1)) ' - r: ' num2str(r)]});
+     title({[fileName] ; ['p: ' num2str(p_values_lambda(end)) ' - l: ' num2str(lambda_values(end)) ' - r: ' num2str(r_tot)]});
     % xlabel(['p: ' num2str(spoc_res(1,2))])
  end 
 topoplot(A(:,1),EEG.chanlocs);
@@ -227,23 +218,34 @@ hold on;
 
 end
 
-% Save the current figure
-%sgtitle(['Spatial patterns of the ' cond{fold} ' condition'])
-saveas(gcf,[spoc_path cond{fold} '/SBA/summaries/SPoC_topo_' cond{fold} '.png']);
+% Save current figure
+%sgtitle(['Spatial patterns of the ' cond{folder} ' condition'])
+saveas(gcf,[spoc_path cond{folder} '/SBA/summaries/SPoC_topo_' cond{folder} '.png']);
+
+%% Group-level stats
+
+% Compute a one-sample, one-sided t.test to assess whether the mean
+% correlation value between z and z_est is sig. lower than 0 (aka
+% negative - coherently with our hypothesis). 
+
+[stats.h,stats.p,stats.ci,stats.stats] = ttest([struct_summ.r],0,'Tail','left');
+
+% Save struct
+save([spoc_path cond{folder} '/SBA/summaries/SPOC_groupstats_'  cond{folder} '.mat'], 'stats');
 
 %%  Save the general summary structure
-%save([spoc_path cond{fold} '/SBA/summaries/SPOC_results_'  cond{fold} '.mat'], 'SPOC_results');
+%save([spoc_path cond{folder} '/SBA/summaries/SPOC_results_'  cond{folder} '.mat'], 'SPOC_results');
 
 table_summ = struct2table(struct_summ);
 table_z = struct2table(struct_z);
 table_zest = struct2table(struct_zest);
 
-save([spoc_path cond{fold} '/SBA/summaries/SPOC_results_'  cond{fold} '.mat'], 'SPOC_results');  % results summary .mat structure
-writetable(table_summ,[spoc_path cond{fold} '/SBA/summaries/_summary.csv']);  % lambda, p.values and corr. coeff summary
+save([spoc_path cond{folder} '/SBA/summaries/SPOC_results_'  cond{folder} '.mat'], 'SPOC_results');  % results summary .mat structure
+writetable(table_summ,[spoc_path cond{folder} '/SBA/summaries/_summary.csv']);  % lambda, p.values and corr. coeff summary
 
 % Save the 2 tables needed for Results/
-writetable(table_z,[results_path cond{fold} '/targetTableSPoC_' cond{fold} '.csv'],'WriteVariableNames',0);  % target z
-writetable(table_zest,[results_path cond{fold} '/predictionTableSPoC_' cond{fold} '.csv'],'WriteVariableNames',0);  % estimated z
+writetable(table_z,[results_path cond{folder} '/targetTableSPoC_' cond{folder} '.csv'],'WriteVariableNames',0);  % target z
+writetable(table_zest,[results_path cond{folder} '/predictionTableSPoC_' cond{folder} '.csv'],'WriteVariableNames',0);  % estimated z
 
 %% Clear all the summary structures and tables and close figures
 clear SPOC_results struct_summ table_summ struct_z struct_zest table_z table_zest;
--- Analysis/Modelling/SPoC/NVR_SPoC_Group.m
+++ Analysis/Modelling/SPoC/NVR_SPoC_Group.m
@@ -1,5 +1,7 @@
 %% NeVRo_SPoC_Group
-% This script allow to compute the group results of SPoC:
+
+% This script allows to compute and plot SpoC's group results:
+
 % 1.Compute the absolute values of each spatial pattern matrix
 % 2.Average each absolute A
 % 3.Plot the comprehensive average plots 
@@ -13,13 +15,15 @@ clear all
 %% Set paths
 
 % Add general matlab scripts and eeglab plugins
-addpath(genpath('/Users/Alberto/Documents/MATLAB/'));
+addpath(genpath('/Users/Alberto/MATLAB/'));
 
-% Open eeglab
+% Open EEGLAB
 [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
 
 % Paths
-master_path = '/Users/Alberto/Documents/PhD/PhD_Side/NeVRo/';
+master_path = '../../../../NeVRo/';
+addpath(master_path);
+
 ssd_path = [master_path 'Data/EEG/07_SSD/'];
 rat_path = [master_path 'Data/ratings/continuous/not_z_scored/']; 
 spoc_path = [master_path 'Data/EEG/08.1_SPOC/']; 
@@ -30,16 +34,16 @@ cond = {'nomov','mov'};
 % Load chanlocs
 load('chans.mat');
 
-% IF you need/want: Fix radius to fit everything "within the scalp"
+% IF you need/want: fix radius to fit everything "within the scalp"
 for n=1:length(chans)
        chans(1,n).radius = chans(1,n).radius*0.795;
 end
 
 %% Load Data
 
-for fold = 1:length(cond)
+for folder = 1:length(cond)
        
-rawDataFiles = dir([spoc_path cond{fold} '/SBA/*res.mat']);  %we specifcally use SBA data
+rawDataFiles = dir([spoc_path cond{folder} '/SBA/*A.mat']);  %we specifcally use SBA data
 
 A_group = ones(32,length(rawDataFiles));
 
@@ -48,39 +52,48 @@ for isub = 1:length(rawDataFiles)
 loadName = rawDataFiles(isub).name;
 fileName = loadName(1:7);
 
-% Load spoc results
+% Load SPoC's results
 load([rawDataFiles(isub).folder '/' rawDataFiles(isub).name]);
 
 % Select the A matrix of the best SPoC component
-A = spoc_res(1,3:end)'; %invert it into col form
-%A_abs = abs(A); %is it correct? NO NEED TO DO IT WITH THE MAGNITUDE
+% A = spoc_res(1,3:end)'; %invert it into col form
 
-%A/magnitude(A) aka:
-A_mag = A/norm(A);
-%then Average of all the single subject A
-%plot it!
+A = A(:,end);
 
-A_group(:,isub) = A;
-%A_group_abs(:,isub) = A_abs;
+% A/magnitude(A) aka:
+A_mag = abs(A/norm(A));
 
+%..then average of all the single subject A
+A_group(:,isub) = abs(A);
 A_group_mag(:,isub) = A_mag;
+
 end 
 
 %A_avg_abs = mean(A_group_abs,2); %mean with absolute values
 A_avg = mean(A_group,2); %mean with all values
 A_avg_mag = mean(A_group_mag,2);
-end 
 
 
-%% Let's plot them
-% Plot
-figure;
+%%  Plot
+
+% Average 
+figure('units','normalized','outerposition',[0 0 1 1]);
 subplot(1,2,1);
 topoplot(A_avg,chans);
-title({'Average Spatial Patterns'})
-colorbar;
+colormap('viridis');
+h = colorbar;
+set(h,'Yticklabel',linspace(min(A_avg),max(A_avg)));
+title(['Average Spatial Patterns - ' cond{folder}]);
 
+% Average magnitude
 subplot(1,2,2);
 topoplot(A_avg_mag,chans);
-title({'Average Spatial Patterns (with magnitude ratio)'})
-colorbar;
+colormap('viridis');
+j = colorbar;
+set(j,'Yticklabel',linspace(min(A_avg_mag),max(A_avg_mag)));
+title(['Average Spatial Patterns (with magnitude ratio) - ' cond{folder}])
+
+saveas(gcf,['avg_topoplots_' cond{folder} '.jpeg']);
+close all;
+
+end 
