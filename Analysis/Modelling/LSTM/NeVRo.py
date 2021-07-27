64945504e3b1137a856d465f6f1cc17eda6d64f3
simon.hofmann <shoffmann@cbs.mpg.de>
2021-07-27 18:48:02 +0200

    mv fct to utils

--- Analysis/Modelling/LSTM/NeVRo.py
+++ Analysis/Modelling/LSTM/NeVRo.py
@@ -798,15 +798,6 @@ def main(_):
                           str(FLAGS.dellog)])
 
 
-def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
-
-
 # %% Main: Run >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
 
 if __name__ == '__main__':
